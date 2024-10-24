b00dffd090bf9ebdd1068ef6e8a98a51bca36673
Juwon-14 <wndnjs9127@gmail.com>
2024-10-24 12:37:10 -0700

    Update main.cpp

--- Activity-25/main.cpp
+++ Activity-25/main.cpp
@@ -2,13 +2,15 @@
 //  main.cpp
 //  Activity-25
 //
-//  Created by 홍주원 on 10/24/24.
+//  Created by Juwon Hong on 10/24/24.
 //
 
-#include <iostream>
+#include "Customer.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    Customer customer;
+    inputCustomerData(customer);
+    printCustomerData(customer);
+
     return 0;
 }
